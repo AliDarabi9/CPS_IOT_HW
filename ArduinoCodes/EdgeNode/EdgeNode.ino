b0d39c5670643a09515ab284b02e761cefad7a89
AliDarabi9 <alidarabi9981@gmail.com>
2025-04-17 14:28:16 +0330

    Add DC MOTOR

--- ArduinoCodes/EdgeNode/EdgeNode.ino
+++ ArduinoCodes/EdgeNode/EdgeNode.ino
@@ -5,40 +5,52 @@
 Servo myservo;
 int moistureSensorValue;
 int servoPosition = 1500;
+int m1 = 13, m2 = 12, en = 3;  // Changed en to pin 3
+int pot = A1, st = 0;
 
-// 20x4 LCD, address 0x27 (try 0x3F if it doesn’t work)
 LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
 void setup() {
-  moistureSensorValue = analogRead(A0);     
-  myservo.attach(9); 
+  pinMode(m1, OUTPUT);
+  pinMode(m2, OUTPUT);
+  pinMode(en, OUTPUT);
+  
+  myservo.attach(9);
   Serial.begin(9600); 
 
-  lcd.init();        // Correct for I2C LCD
-  delay(100);        // Let it initialize properly
-  lcd.backlight();   // Turn on the backlight
+  lcd.init();
+  delay(100);
+  lcd.backlight();
   lcd.print("System Ready");
-  delay(2000);
+  delay(500);
   lcd.clear();
 }
 
-
 void loop() {
-  float SensorVolts = analogRead(A0) * 0.0048828125;
+  moistureSensorValue = analogRead(A0);
+  float SensorVolts = moistureSensorValue * 0.0048828125;
+
   int angle = map(servoPosition, 1000, 2000, 0, 180);
+  myservo.write(angle);
 
-  Serial.print("Servo Position: ");
-  Serial.println(angle);
+  st = analogRead(pot) / 2;  
+  analogWrite(en, st);
+  digitalWrite(m1, HIGH);
+  digitalWrite(m2, LOW);
 
+  // LCD Display
   lcd.setCursor(0, 0);
-  lcd.print("Moisture:");
+  lcd.print("Moisture: ");
   lcd.print(SensorVolts, 2);
 
   lcd.setCursor(0, 1);
-  lcd.print("Servo:");
+  lcd.print("Servo: ");
   lcd.print(angle);
-  lcd.print((char)223); // degree symbol
+  lcd.print((char)223);
 
-  myservo.write(servoPosition);
-  delay(500);
+  lcd.setCursor(0, 2);
+  lcd.print("Motor Speed: ");
+  lcd.print(st);
+
+  delay(100);
 }
